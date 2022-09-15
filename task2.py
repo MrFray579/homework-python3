f430a9c2ae267d5416405eb0145bef50c7d1949c
Fray <kozar20000@mail.ru>
2022-09-15 15:00:24 +0300

    add

--- task2.py
+++ task2.py
@@ -8,3 +8,14 @@ arr2 = list()
 for i in range(math.ceil(len(arr)/2 )):
     arr2.append(arr[i] + arr[-i-1])
 print(arr2)
+
+# 2 вариант
+list_1 = [3, 2, 4, 5, 6]
+len_list = 0
+if len(list_1) % 2 == 0:
+    len_list - len(list_1)//2
+else:
+    len_list = len(list_1)//2 + 1
+
+for i in range (len_list):
+    print(list_1[i] * list_1[len(list_1)-1-i])
